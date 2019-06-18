259efd6e37d5af18bb44dc71530dd07dce023246
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-06-18 12:21:53 -0600

    Fixes that lowercase error

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Header from "../components/header"
+import Header from "../components/Header"
 import Navbar from "../components/Navbar"
 
 import { FaLinkedin, FaEnvelopeOpenText, FaGithub } from 'react-icons/fa'
